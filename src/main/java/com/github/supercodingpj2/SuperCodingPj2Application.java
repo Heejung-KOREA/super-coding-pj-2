a1aabc0a244aa1ac3fb5939437cb31b3a71f2c99
Heejung-KOREA <heejungs0304@gmail.com>
2024-05-21 17:15:56 +0900

    초기 환경설정

---
+++ src/main/java/com/github/supercodingpj2/SuperCodingPj2Application.java
@@ -0,0 +1,13 @@
+package com.github.supercodingpj2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SuperCodingPj2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SuperCodingPj2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/github/supercodingpj2/SuperCodingPj2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.supercodingpj2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SuperCodingPj2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
